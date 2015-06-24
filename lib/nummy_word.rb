b0228fa133c70aa637490e4273fe6c7eb64bc61f
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2015-06-24 08:51:00 +0100

    feat(numerals): allow the return of two digit numbers
    All two-digit numbers can now be returned as it's word form.
    
    The `numerals` method now returns a hash, as the remainder of the
    hardcoded numbers were no longer in order.

--- lib/nummy_word.rb
+++ lib/nummy_word.rb
@@ -21,28 +21,35 @@ class NummyWord
   end
 
   def numerals
-    [
-      'zero',
-      'one',
-      'two',
-      'three',
-      'four',
-      'five',
-      'six',
-      'seven',
-      'eight',
-      'nine',
-      'ten',
-      'eleven',
-      'twelve',
-      'thirteen',
-      'fourteen',
-      'fifteen',
-      'sixteen',
-      'seventeen',
-      'eighteen',
-      'nineteen',
-      'twenty'
-    ]
+    {
+      0 => 'zero',
+      1 => 'one',
+      2 => 'two',
+      3 => 'three',
+      4 => 'four',
+      5 => 'five',
+      6 => 'six',
+      7 => 'seven',
+      8 => 'eight',
+      9 => 'nine',
+      10 => 'ten',
+      11 => 'eleven',
+      12 => 'twelve',
+      13 => 'thirteen',
+      14 => 'fourteen',
+      15 => 'fifteen',
+      16 => 'sixteen',
+      17 => 'seventeen',
+      18 => 'eighteen',
+      19 => 'nineteen',
+      20 => 'twenty',
+      30 => 'thirty',
+      40 => 'forty',
+      50 => 'fifty',
+      60 => 'sixty',
+      70 => 'seventy',
+      80 => 'eighty',
+      90 => 'ninety',
+    }
   end
 end
--- spec/nummy_word_spec.rb
+++ spec/nummy_word_spec.rb
@@ -11,12 +11,39 @@ describe NummyWord do
     end
   end
 
-  context "when passed a number between 21 and 29" do
-    numerals = ['twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine']
+  describe "returning composed numerals" do
+    context "when passed a number between 21 and 29" do
+      numerals = ['twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine']
 
-    (21..29).each do |i|
-      it "can return the english for #{i}" do
-        expect(NummyWord.new(i).word).to eq(numerals[i-21])
+      (21..29).each do |i|
+        it "can return the english for #{i}" do
+          expect(NummyWord.new(i).word).to eq(numerals[i-21])
+        end
+      end
+    end
+
+    context "when passed a number between 30 and 99" do
+      numerals = {
+        30 => 'thirty',
+        31 => 'thirty-one',
+        40 => 'forty',
+        42 => 'forty-two',
+        50 => 'fifty',
+        54 => 'fifty-four',
+        60 => 'sixty',
+        66 => 'sixty-six',
+        70 => 'seventy',
+        77 => 'seventy-seven',
+        80 => 'eighty',
+        88 => 'eighty-eight',
+        90 => 'ninety',
+        99 => 'ninety-nine'
+      }
+
+      numerals.each do |k, v|
+        it "can return the english for #{k}" do
+          expect(NummyWord.new(k).word).to eq(v)
+        end
       end
     end
   end
