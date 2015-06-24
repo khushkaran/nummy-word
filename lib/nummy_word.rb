a388989b9ab1f1e5ac2c6dc586c8b91c5036861a
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2015-06-24 07:55:25 +0100

    feat(numerals): allow numbers to be composed
    There are enough hardcoded numbers to allow for the return of numbers 21
    to 29. These are composed from their counterparts in the numerals array.

--- lib/nummy_word.rb
+++ lib/nummy_word.rb
@@ -5,10 +5,20 @@ class NummyWord
   end
 
   def word
-    numerals[@number]
+    numerals[@number] ? numerals[@number] : compose_numeral
   end
 
   private
+
+  def compose_numeral
+    stringified_number = @number.to_s
+    zero_counter = stringified_number.length
+    stringified_number.chars.map{ |char|
+      zero_counter -= 1
+      char += "0" * zero_counter
+      numerals[char.to_i]
+    }.join('-')
+  end
 
   def numerals
     [
--- spec/nummy_word_spec.rb
+++ spec/nummy_word_spec.rb
@@ -1,11 +1,23 @@
 require 'nummy_word'
 
 describe NummyWord do
-  zero_twenty_numerals = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty' ]
+  context "when passed number between 0 and 20" do
+    numerals = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty' ]
 
-  (0..20).each do |i|
-    it "can return the english for #{i}" do
-      expect(NummyWord.new(i).word).to eq(zero_twenty_numerals[i])
+    (0..20).each do |i|
+      it "can return the english for #{i}" do
+        expect(NummyWord.new(i).word).to eq(numerals[i])
+      end
+    end
+  end
+
+  context "when passed a number between 21 and 29" do
+    numerals = ['twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine']
+
+    (21..29).each do |i|
+      it "can return the english for #{i}" do
+        expect(NummyWord.new(i).word).to eq(numerals[i-21])
+      end
     end
   end
 end
