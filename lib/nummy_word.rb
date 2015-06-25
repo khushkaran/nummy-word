6707da9fd2928290fad822ad53cb9916e87438af
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2015-06-25 08:03:05 +0100

    feat(convert): return numeral for numbers 3 through 9
    Using the numeral constant, the numeral for single digit numbers is
    returned.

--- lib/nummy_word.rb
+++ lib/nummy_word.rb
@@ -1,5 +1,12 @@
 class NummyWord
   NUMERALS = {
+    9 => 'nine',
+    8 => 'eight',
+    7 => 'seven',
+    6 => 'six',
+    5 => 'five',
+    4 => 'four',
+    3 => 'three',
     2 => 'two',
     1 => 'one'
   }
--- spec/nummy_word_spec.rb
+++ spec/nummy_word_spec.rb
@@ -12,4 +12,10 @@ describe NummyWord do
   it 'returns two when 2 is passed in' do
     expect(NummyWord.new(2).to_words).to eq('two')
   end
+
+  { 9 => 'nine', 8 => 'eight', 7 => 'seven', 6 => 'six', 5 => 'five', 4 => 'four', 3 => 'three' }.each do |num, numeral|
+    it "returns #{numeral} when #{num} is passed in" do
+      expect(NummyWord.new(num).to_words).to eq(numeral)
+    end
+  end
 end
